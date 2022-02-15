35c27ede68f58dca0fa65d55e40019df17179c4a
Braden Rich <bradenwrich@gmail.com>
2022-02-14 19:15:16 -0500

    Implement Latitude and Longitude

--- WeatherDetector/lib/coordinate_lookup.rb
+++ WeatherDetector/lib/coordinate_lookup.rb
@@ -5,6 +5,34 @@ require 'net/http'
 
 class CoordinateLookup
   def initialize(location)
-    @location ||= location
+    @location = location
+  end
+
+  def location
+    @location ||= WeatherLocation.new.formatted_location
+  end
+
+  def fetch_results
+    @fetch_results ||= begin
+      uri = URI('https://geocode.xyz')
+      uri.query = params
+      JSON.parse(Net::HTTP.get(uri))
+    end
+  end
+
+  def longitude
+    @longitude ||= fetch_results['longt']
+  end
+
+  def latitude
+    @latitude ||= fetch_results['latt']
+  end
+
+  def params 
+    URI.encode_www_form({
+      # 'auth' => 'your auth code',
+      'locate' => location,
+      'geoit' => 'json'
+    })
   end
 end
--- WeatherDetector/lib/retrieve_weather.rb
+++ WeatherDetector/lib/retrieve_weather.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'weather_location'
+require_relative 'coordinate_lookup'
 
 pp WeatherLocation.new.ip, WeatherLocation.new.country, WeatherLocation.new.city
+# pp CoordinateLookup.new('Knoxville').fetch_results['longt'], CoordinateLookup.new('Knoxville').fetch_results['latt']
+pp CoordinateLookup.new('Knoxville').longitude, CoordinateLookup.new('Knoxville').latitude
